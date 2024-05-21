85d3aa38e2235361634a941ff535ecdf29103a69
primo <sim.merdedouch@gmail.com>
2024-05-21 23:33:41 +0100

    fetching data from backend cities

--- src/Components/City/Citys.jsx
+++ src/Components/City/Citys.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import { Stack, Card, Grid, CardHeader, CardContent } from '@mui/material'
 import CityList from '../City/CityList'
-
+import { fetchCities } from '../../api/api'
 const Citys = () => {
-    
+    const [cities,setCities] = useState([]);
+    const [loading,setLoading] = useState(true);
+    const [error,setError] = useState([]);
+
+
+    useEffect(() => {
+      const getCities = async () => {
+        try {
+          const data = await fetchCities();
+          setCities(data._embedded.cities);
+          setLoading(false);
+        } catch (error) {
+          console.log(error);
+          setError(error);
+          setLoading(false);
+        }
+      }
+      getCities();
+    }, []);
+
+
   return (
     <Card>          
         <CardHeader title="City" className='cardHeader' 
@@ -11,7 +31,7 @@ const Citys = () => {
       </CardHeader>
         <CardContent >
                 <Stack>
-                <CityList></CityList>
+                <CityList cities={cities}></CityList>
                 </Stack>
       </CardContent>
     </Card>
