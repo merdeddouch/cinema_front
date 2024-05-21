b699c9666ed9e51212cfbdfd2e7ea1ef8c4e0c31
primo <sim.merdedouch@gmail.com>
2024-05-22 00:06:23 +0100

    adding marging and padding

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,8 +5,7 @@ import Citys from '../City/Citys'
 
 const Home = () => {
   return (
-    <Grid container spacing={2}>
-
+    <Grid container spacing={2} mt={3} px={2}>
         <Grid item xs={12} md={4}>
           <Citys></Citys>
         </Grid>
