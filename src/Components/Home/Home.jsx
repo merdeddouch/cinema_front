98ef09afb5acda6554931fad1980d344be45c816
primo <sim.merdedouch@gmail.com>
2024-05-21 17:19:50 +0100

    updated the home component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Stack, Card, Grid, CardHeader, CardContent } from '@mui/material'
+import CityList from '../City/CityList'
+import Citys from '../City/Citys'
 
 const Home = () => {
   return (
-    <div>
-      this is home hh
-    </div>
+    <Grid container spacing={2}>
+
+        <Grid item xs={12} md={4}>
+          <Citys></Citys>
+        </Grid>
+        <Grid item xs={12} md={7}>
+          Cinemas
+        </Grid>
+    </Grid>
   )
 }
 
