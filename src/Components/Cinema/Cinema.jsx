564e51481998a983bfe63851499aa8acb28ad25b
primo <sim.merdedouch@gmail.com>
2024-05-21 23:29:06 +0100

    fetching data from backend cinemas

--- src/Components/Cinema/Cinema.jsx
+++ src/Components/Cinema/Cinema.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetchCinemas } from '../../api/api'
 
 const Cinema = () => {
+  const [cinemas,setCinema] = useState([]);
+  const [loading,setLoding] = useState(true);
+  const [errorFetching,setErrorFetching] = useState([]);
+
+  useEffect(() => {
+    const getCinemas = async () => {
+        try {
+          const data = await fetchCinemas()
+          console.log(data._embedded.cinemas);
+          setCinema(data._embedded.cinemas)
+        } catch (error) {
+          console.log(`errors while feching : ${error}`);
+        }
+    }
+
+    getCinemas();
+  }, []);
+
+
+
   return (
-    <div>
-      ana cinema
-    </div>
+<div>
+            <h1>Cinemas</h1>
+            <ul>
+                {cinemas.map((cinema,index) => (
+                    <li key={index}>{cinema.name}</li>
+                ))}
+            </ul>
+        </div>
   )
 }
 
