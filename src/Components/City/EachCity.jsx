d556f6caffbc749bd036f2f6946d1f2a01a81b14
primo <sim.merdedouch@gmail.com>
2024-05-22 23:02:56 +0100

    added url prop and a handlClick

--- src/Components/City/EachCity.jsx
+++ src/Components/City/EachCity.jsx
@@ -1,13 +1,15 @@
 import { Button } from '@mui/material'
 import React from 'react'
 
-const EachCity = ({cityName}) => {
+const EachCity = ({cityName,url_link,handlClick,}) => {
   return (  
     <Button variant='text' 
+    color='primary'
     sx={{
         fontSize:"20px",
         height:"50px",
     }}
+    onClick={() => handlClick(url_link)}
     >{cityName}</Button>
   )
 }
