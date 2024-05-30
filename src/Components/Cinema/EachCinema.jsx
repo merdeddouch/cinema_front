83b9355ad9914921d114ffc84ac4ce1549028c9a
primo <sim.merdedouch@gmail.com>
2024-05-30 18:55:03 +0100

    added a handler click

--- src/Components/Cinema/EachCinema.jsx
+++ src/Components/Cinema/EachCinema.jsx
@@ -1,11 +1,17 @@
 import { Button } from '@mui/material'
-import React from 'react'
+import React,{useContext} from 'react'
 import './cinemaStyle.css'
+import { cities_cinema_context } from '../../contex/contexApp'
 
 
-const EachCinema = ({cinema}) => {
+const EachCinema = ({cinema,id}) => {
+  const {setCurrentCinemaActive,currentCinemaActive} = useContext(cities_cinema_context);
+  const handleClickCinema = (vr_id) =>{
+    setCurrentCinemaActive(vr_id)
+    //console.log(`currentCinema: ${currentCinemaActive}`);
+  }
   return (
-    <Button disableElevation className='cinemaInCityOption' variant='contained' size='medium'>
+    <Button disableElevation className={`cinemaInCityOption ${(id == currentCinemaActive)? "CinemaActive" :''}`} variant='contained' size='medium' id={id} onClick={() => handleClickCinema(id)}>
       {cinema}
     </Button>
   )
