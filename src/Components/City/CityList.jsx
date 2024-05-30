73e1af932796061fc97dcc4eb39756eb5d31f937
primo <sim.merdedouch@gmail.com>
2024-05-30 16:01:24 +0100

    added some props

--- src/Components/City/CityList.jsx
+++ src/Components/City/CityList.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import EachCity from './EachCity';
 
-const CityList = ({cities,handleClick}) => {
+const CityList = ({cities,handleClick,currentCityActive}) => {
   return (
     cities.map((city,index) => {
-      return <EachCity  cityName={city.name} key={index} url_link={city._links.cinemas.href} handlClick={handleClick}></EachCity>
+      return <EachCity  cityName={city.name} key={index} id={index} currentCityActive={currentCityActive} url_link={city._links.cinemas.href} handlClick={handleClick}></EachCity>
     })
 
   )
