afa7f859d77f632d7e6bca54c4f7da9323863daa
primo <sim.merdedouch@gmail.com>
2024-06-02 06:23:29 +0100

    pass the function to get rooms throught the cinema as a props

--- src/Components/Cinema/ListofCinemaInCity.jsx
+++ src/Components/Cinema/ListofCinemaInCity.jsx
@@ -2,12 +2,12 @@ import { Stack } from '@mui/material'
 import React from 'react'
 import EachCinema from './EachCinema'
 
-const ListofCinemaInCity = ({cinemasInCity}) => {
+const ListofCinemaInCity = ({cinemasInCity,getRoomsIncinema}) => {
   return (
     <Stack direction={"row"} spacing={1} m={2}>
       {
-        cinemasInCity.map((cinema,index) =>{
-            return <EachCinema cinema={cinema.name} key={index} id={index}></EachCinema>
+        cinemasInCity?.map((cinema,index) =>{
+            return <EachCinema cinema={cinema.name} url_roomsInCinema = {cinema._links.rooms.href} key={index} id={index} getRoomsIncinema={getRoomsIncinema}></EachCinema>
         })
       }
     </Stack>
