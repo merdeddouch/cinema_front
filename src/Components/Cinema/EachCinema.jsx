33b5fbbfc579a8670b0c1eea60c0d9e6bda3c4ae
primo <sim.merdedouch@gmail.com>
2024-06-02 06:24:22 +0100

    updated the cinema button to fetch the rooms

--- src/Components/Cinema/EachCinema.jsx
+++ src/Components/Cinema/EachCinema.jsx
@@ -1,15 +1,20 @@
-import { Button } from '@mui/material'
+import { Button,Card } from '@mui/material'
 import React,{useContext} from 'react'
 import './cinemaStyle.css'
 import { cities_cinema_context } from '../../contex/contexApp'
 
 
-const EachCinema = ({cinema,id}) => {
+const EachCinema = ({cinema,id,url_roomsInCinema,getRoomsIncinema}) => {
+
+
+
   const {setCurrentCinemaActive,currentCinemaActive} = useContext(cities_cinema_context);
   const handleClickCinema = (vr_id) =>{
     setCurrentCinemaActive(vr_id)
+    getRoomsIncinema(url_roomsInCinema)
     //console.log(`currentCinema: ${currentCinemaActive}`);
   }
+
   return (
     <Button disableElevation className={`cinemaInCityOption ${(id == currentCinemaActive)? "CinemaActive" :''}`} variant='contained' size='medium' id={id} onClick={() => handleClickCinema(id)}>
       {cinema}
