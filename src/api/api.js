8644708bd174e5ca573440ebe79ed39a45a06ecc
primo <sim.merdedouch@gmail.com>
2024-06-02 06:17:00 +0100

    added new functions to fetch rooms in cincemna

--- src/api/api.js
+++ src/api/api.js
@@ -31,3 +31,14 @@ export const fetchCinemasByCity = async (url) => {
         throw error; 
     }
 };
+
+export const fetchRoomsInCinema = async (url) => {
+    try {
+        const response = await axios.get(url);
+        // console.log(response.data)
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching rooms in cinema:', error);
+        throw error;
+    }
+}
